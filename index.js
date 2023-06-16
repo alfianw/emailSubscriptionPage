71a268d4b0b8b1f589a189c0c322df3997c574a9
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-06-16 10:52:03 +0700

    done dalam bentuk dekstop

--- index.js
+++ index.js
@@ -14,4 +14,8 @@ function validasi() {
 function myFunction() {
     document.getElementById("button2").style.background = "hsl(235, 18%, 26%)";
     document.getElementById("button2").style.boxShadow = "none";
+
+    setTimeout(function() {
+        window.location.assign('index.html');
+      }, 3000); // 2000 milidetik = 2 detik
 }
