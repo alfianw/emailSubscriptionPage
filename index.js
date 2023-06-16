c6b235c62cf1c32010250d69fb9398ad67e24e37
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-06-16 10:45:32 +0700

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+function validasi() {
+    const email = document.getElementById("email").value;
+    
+    if (email != "") {
+        console.log(email)
+        window.location.assign('sukses.html');
+        localStorage.setItem('email', email);
+        window.location.href = 'sukses.html';
+    } else {
+        document.getElementById("warning").style.display = "block";
+    }
+}
+
+function myFunction() {
+    document.getElementById("button2").style.background = "hsl(235, 18%, 26%)";
+    document.getElementById("button2").style.boxShadow = "none";
+}
